de5b8cf475c8c909a878188d4e96a79bdd2231c9
sauvageb <sauvageboris.pro@gmail.com>
2023-03-31 09:11:57 +0200

    correction drumkit

---
+++ 3_drumkit/index.js
@@ -0,0 +1,54 @@
+for (var i = 0; i <= 6; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        makeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+
+
+}
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(key);
+    }
+}
+
+function buttonAnimation(key) {
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function () {
+        document.querySelector("." + key).classList.remove("pressed");
+    }, 100);
+}
